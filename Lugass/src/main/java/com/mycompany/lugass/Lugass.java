0dfada139a04f32428ba979cdbaffc599b34ef02
BERKABZ <150333064+BERKABZ@users.noreply.github.com>
2023-11-22 04:47:40 +0800

    Add files via upload

---
+++ Lugass/src/main/java/com/mycompany/lugass/Lugass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lugass;
+
+/**
+ *
+ * @author Edward
+ */
+public class Lugass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
